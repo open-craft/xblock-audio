ae174af6830b69f32eca6eb6e2bfd97c06e281b9
Samuel Allan <samuel@opencraft.com>
2025-08-18 16:37:56 +0930

    chore: remove delete transcript checkbox
    The UI/UX of this still needs discussion.
    In the meantime, remove the checkbox.
    
    Private-ref: https://tasks.opencraft.com/browse/BB-9935

--- audio/audio.py
+++ audio/audio.py
@@ -151,15 +151,14 @@ class AudioBlock(AudioFields, StudioEditableXBlockMixin, StudioContainerXBlockMi
         storage = get_storage_backend()
         will_upload_transcript_file = 'transcript_file' in data and hasattr(data.get('transcript_file'), 'file')
 
-        if data.get("delete_transcript_file", "") == "on" or will_upload_transcript_file:
+        if will_upload_transcript_file:
+            # clean up any existing file first
             if self.transcript_file and storage.exists(self.transcript_file):
                 storage.delete(self.transcript_file)
             self.transcript_file = None
 
-        if will_upload_transcript_file:
+            # generate a safe path for the transcript file and save it
             transcript_file = data['transcript_file']
-
-            # generate a safe path for the transcript file
             name = get_valid_filename(transcript_file.filename)
             safe_usage_key = get_valid_filename(self.usage_key)
             file_path = f"{safe_usage_key}/transcripts/{name}"
